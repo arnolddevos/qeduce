dffb5e05ff09e15ef4990528c1dd902fb2273e8d
Arnold deVos <adv@langdale.com.au>
2016-02-01 21:22:19 +1100

    Added SQLTerm and SQLRowView

--- src/main/scala/qeduce/Qeduce.scala
+++ src/main/scala/qeduce/Qeduce.scala
@@ -32,11 +32,16 @@ trait Qeduce {
 
   implicit class SQLofValue[A](a: A)(implicit t: SQLType[A]) extends SQL {
     val parts = Seq("", "")
-    val params = Seq(new SQLValueConcrete(a))
+    val params = Seq(new SQLCapture(a))
   }
 
   implicit class SQLofSymbol(s: Symbol) extends SQL {
     val parts = Seq("\""+s.name+"\"")
+    val params = Seq()
+  }
+
+  implicit class SQLofTerm(t: SQLTerm[_]) extends SQL {
+    val parts = Seq("\""+t.name+"\"")
     val params = Seq()
   }
 
@@ -48,15 +53,34 @@ trait Qeduce {
   }
 
   trait SQLValue {
-    type A
-    def value: A
-    def sqlType: SQLType[A]
+    type Value
+    def value: Value
+    def sqlType: SQLType[Value]
     override def toString = "${"+sqlType.display(value)+"}"
   }
 
-  implicit class SQLValueConcrete[A1](a: A1)(implicit t: SQLType[A1]) extends SQLValue {
-    type A = A1
+  implicit class SQLCapture[A](a: A)(implicit t: SQLType[A]) extends SQLValue {
+    type Value = A
     val value = a
     val sqlType = t
+  }
+
+  trait SQLTerm[A] {
+    def name: String
+    def sqlType: SQLType[A]
+    def apply(rs: ResultSet): A = sqlType.extract(rs, name)
+    override def toString = "'" + name
+  }
+
+  def term[A](n: String)(implicit t: SQLType[A]): SQLTerm[A] = new SQLTerm[A] {
+    val name = n
+    val sqlType = t
+  }
+
+  def term[A](s: Symbol)(implicit t: SQLType[A]): SQLTerm[A] = term(s.name)
+
+  implicit class SQLRowView(rs: ResultSet) {
+    def apply[A](c: Symbol)( implicit t: SQLType[A]): A = t.extract(rs, c.name)
+    def get[A](c: Symbol)( implicit t: SQLType[Option[A]]): Option[A] = t.extract(rs, c.name)
   }
 }
